4354da13922f1afd00ea69bf368dd3532bf72785
Darwin <darwin78913@gmail.com>
2023-05-08 22:06:37 -0400

    day night cycle

--- src/components/lights/BasicLights.js
+++ src/components/lights/BasicLights.js
@@ -1,23 +1,40 @@
-import { Group, SpotLight, AmbientLight, HemisphereLight } from 'three';
+import { Group, SpotLight, AmbientLight, HemisphereLight, HemisphereLightHelper, DirectionalLight } from 'three';
+import { SECONDS_PER_DAY } from '../scenes/Main';
+
 
 class BasicLights extends Group {
-    constructor(...args) {
+    constructor(bg, dayMat, nightMat, ...args) {
         // Invoke parent Group() constructor with our args
         super(...args);
-
-        const dir = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
-        const dir2 = new SpotLight(0xffffff, 1.6, 7, 0.8, 1, 1);
         const ambi = new AmbientLight(0x404040, 1.32);
-        const hemi = new HemisphereLight(0xffffbb, 0x080820, 2.3);
 
-        dir.position.set(5, 1, 2);
-        dir.target.position.set(0, 0, 0);
+        this.dayMat = dayMat;
+        this.nightMat = nightMat;
+        this.backgroundMesh = bg;
+        this.sunlight = new DirectionalLight(0xedcc72, 0.7);
+        this.daytime = true;
 
-        dir2.position.set(-5, -1, -2);
-        dir2.target.position.set(0, 0, 0);
+        this.add(ambi, this.sunlight);
+    }
 
+    update(timestamp) {
+        const timeOfDay = timestamp/1000/SECONDS_PER_DAY;
+        this.sunlight.position.setY(Math.cos(timeOfDay));
+        this.sunlight.position.setZ(Math.sin(timeOfDay));
 
-        this.add(ambi, dir, hemi, dir2);
+        // if (timeOfDay%SECONDS_PER_DAY < SECONDS_PER_DAY/2) {
+        //     // day time
+        //     if (!this.daytime) {
+        //         this.backgroundMesh.material = this.dayMat;
+        //         this.daytime = true;
+        //     }
+        // } else {
+        //     // night time
+        //     if (this.daytime) {
+        //         this.backgroundMesh.material = this.nightMat;
+        //         this.daytime = false;
+        //     }
+        // }
     }
 }
 
--- src/components/scenes/Main.js
+++ src/components/scenes/Main.js
@@ -1,5 +1,5 @@
 import * as Dat from 'dat.gui';
-import { Scene, Color, ArrowHelper, Vector3, AxesHelper, TextureLoader, RepeatWrapping, ClampToEdgeWrapping, MirroredRepeatWrapping, CubeTextureLoader, Fog, MeshBasicMaterial, FogExp2 } from 'three';
+import { Scene, Color, ArrowHelper, Vector3, AxesHelper, TextureLoader, RepeatWrapping, ClampToEdgeWrapping, MirroredRepeatWrapping, CubeTextureLoader, Fog, MeshBasicMaterial, FogExp2, MeshDistanceMaterial, MeshNormalMaterial, MeshLambertMaterial } from 'three';
 import { Flower, Land } from 'objects';
 import { BasicLights } from 'lights';
 import { SphereGeometry, MeshToonMaterial, Mesh, Euler } from 'three';
@@ -9,6 +9,7 @@ import AsteroidManager from './AsteroidManager';
 
 const SPHERE_RADIUS = 200;
 const UP_VECTOR = new Vector3(0, 1, 0);
+const SECONDS_PER_DAY = 5;
 
 class MainScene extends Scene {
     constructor() {
@@ -18,26 +19,30 @@ class MainScene extends Scene {
         // set the space-sphere bounding box
         this.fog = new Fog(0xcccccc, 0, 350); // fog makes the background look cooler + asteroids fade away instead of disappearing
 
-        const spaceTexture = new TextureLoader().load("src/components/textures/nebula.png");
+        const nebulaTexture = new TextureLoader().load("src/components/textures/nebula.png");
         const backgroundSphere = new SphereGeometry(SPHERE_RADIUS, 40, 20);
-        const backgroundMat = new MeshBasicMaterial({map: spaceTexture});
+        const backgroundMat = new MeshToonMaterial({map: nebulaTexture}); // toon material causes the weird shading
+        const dayMat = new MeshToonMaterial({map: nebulaTexture});
+        const nightMat = new MeshBasicMaterial({map: nebulaTexture});
+
         const backgroundMesh = new Mesh(backgroundSphere, backgroundMat);
         backgroundSphere.scale(-1, 1, 1);
         this.add(backgroundMesh);
 
+        // TODO: add slow auto rotation?
         this.planet = new Planet("Planet1");
 
         // X is red, Y is green, Z is blue
         this.planet.add(new AxesHelper(5));
 
-        const lights = new BasicLights();
+        this.lights = new BasicLights(backgroundMesh, dayMat, nightMat);
 
         // ASTEROID TESTING
         // this.asteroid = new Asteroid("Asteroid1");
         // this.add(this.asteroid);
         this.asteroids = new AsteroidManager(30, 100);
 
-        this.add(lights, this.planet, this.asteroids);
+        this.add(this.lights, this.planet, this.asteroids);
     }
 
     plantFlower(pos, face) {
@@ -54,9 +59,11 @@ class MainScene extends Scene {
 
     update(timestamp) {
         // updates 60 times a second
-        this.asteroids.update(timestamp)
+        this.asteroids.update(timestamp);
+        this.lights.update(timestamp);
     }
 
 }
 
 export default MainScene;
+export {SECONDS_PER_DAY};
