bae22e6e0c8b02312c99a1ddaec2a95bc05d620b
rafdkg <rdavila754@gmail.com>
2023-05-09 20:35:49 -0400

    final? ui scaling

--- src/components/scenes/UIScene.js
+++ src/components/scenes/UIScene.js
@@ -20,37 +20,37 @@ class UIScene extends Scene {
 
         // Current Flower Display UI Elements
 
-        const currFlower = new RoundedButton(camX*.5, camY*.8, 1, 0xc072d4, "currFlower");
+        const currFlower = new RoundedButton(camX*.4, camX*.4, 1, 0xc072d4, "currFlower");
         currFlower.SetPosition(-camX + currFlower.width/2, -camY + currFlower.length/2, 0);
         this.add( currFlower );
 
-        const flowerZero = new RoundedButton(currFlower.width*.8, currFlower.width*.8, 2, 0xff0000, "flowerZero");
+        const flowerZero = new RoundedButton(currFlower.width*.9, currFlower.width*.9, 2, 0xff0000, "flowerZero");
         flowerZero.SetPosition(currFlower.position.x, currFlower.position.y, 0);
         this.add( flowerZero );
         
         // Flower Selection UI elements
 
-        const flowerSelect = new RoundedButton(camX*.75, camY/3, 1, 0xc072d4, "flowerSelect");
-        flowerSelect.SetPosition(-camX/7, -camY + flowerSelect.length/2, 0);
+        const flowerSelect = new RoundedButton(camX*.6, camY/3, 1, 0xc072d4, "flowerSelect");
+        flowerSelect.SetPosition(-camX/3, -camY + flowerSelect.length/2, 0);
         this.add( flowerSelect );
 
-        let fbuttonlen = flowerSelect.width*.2;
+        let fbuttonlen = flowerSelect.length*.8;
 
         const flowerOne = new RoundedButton(fbuttonlen, fbuttonlen, 1, 0xff0000, "flowerOne");
-        flowerOne.SetPosition(flowerSelect.position.x - (flowerSelect.width*.35), -camY + flowerSelect.length/2, 1);
+        flowerOne.SetPosition(flowerSelect.position.x - (flowerSelect.width*.3), -camY + flowerSelect.length/2, 1);
         this.add( flowerOne );
 
         const flowerTwo = new RoundedButton(fbuttonlen, fbuttonlen, 1, 0x00ff00, "flowerTwo");
-        flowerTwo.SetPosition(flowerSelect.position.x - flowerSelect.width*.115, -camY + flowerSelect.length/2, 1);
+        flowerTwo.SetPosition(flowerSelect.position.x - flowerSelect.width*0, -camY + flowerSelect.length/2, 1);
         this.add( flowerTwo );
 
         const flowerThree = new RoundedButton(fbuttonlen, fbuttonlen, 1, 0x0000ff, "flowerThree");
-        flowerThree.SetPosition(flowerSelect.position.x + flowerSelect.width*.115, -camY + flowerSelect.length/2, 1);
+        flowerThree.SetPosition(flowerSelect.position.x + flowerSelect.width*.3, -camY + flowerSelect.length/2, 1);
         this.add( flowerThree );
 
-        const flowerFour = new RoundedButton(fbuttonlen, fbuttonlen, 1, 0x00ffff, "flowerFour");
-        flowerFour.SetPosition(flowerSelect.position.x + (flowerSelect.width*.35), -camY + flowerSelect.length/2, 1);
-        this.add( flowerFour );
+        // const flowerFour = new RoundedButton(fbuttonlen, fbuttonlen, 1, 0x00ffff, "flowerFour");
+        // flowerFour.SetPosition(flowerSelect.position.x + (flowerSelect.width*.35), -camY + flowerSelect.length/2, 1);
+        // this.add( flowerFour );
 
         // Planet selection UI Elements
 
@@ -77,13 +77,6 @@ class UIScene extends Scene {
         this.state.updateList.push(object);
     }
 
-    // update(window) {
-
-    //     // Call update for each object in the updateList
-    //     for (const obj of updateList) {
-    //         obj.update(timeStamp);
-    //     }
-    // }
 }
 
 export default UIScene;
